2ab59e37af91bdcf4200a44c905e0134741378c3
Anderson Torres <anderson.torres.8519@gmail.com>
2025-07-06 13:21:16 -0300

    Fix compilation with gcc14
    Fixing compilation with gcc 14 by adding necessary headers and rewriting SETF_TIME.
    
    Co-authored-by: savalet <savinien.petitjean@gmail.com>

--- src/unzoo.c
+++ src/unzoo.c
@@ -244,8 +244,11 @@
 *H
 */
 #include        <stdio.h>
+#include        <stdlib.h>
 #include        <string.h>
 
+#include        <sys/stat.h>
+#include        <utime.h>
 
 /****************************************************************************
 **
@@ -614,8 +617,8 @@ char            Cmd [256];
 **  members with the correct time as stored in the archive.
 */
 #ifdef  SYS_IS_UNIX
-unsigned int   Secs [2];
-#define SETF_TIME(patl,secs)    (Secs[0]=Secs[1]=(secs),!utime((patl),Secs))
+struct utimbuf Secs;
+#define SETF_TIME(patl,secs) (Secs.actime = Secs.modtime = (secs), !utime((patl), &Secs))
 #endif
 #ifdef  SYS_IS_DOS_DJGPP
 unsigned long   Secs [2];
